37fc0a7330cf6772ccdee65619772be18c541751
921668753 <921668753@qq.com>
2018-08-17 18:59:27 +0800

    fix BUG

--- SillyChildClient/app/src/main/java/com/sillykid/app/homepage/boutiqueline/LineDetailsPayOrderActivity.java
+++ SillyChildClient/app/src/main/java/com/sillykid/app/homepage/boutiqueline/LineDetailsPayOrderActivity.java
@@ -1,6 +1,11 @@
 package com.sillykid.app.homepage.boutiqueline;
 
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import com.common.cklibrary.common.BaseActivity;
+import com.common.cklibrary.common.BindView;
 import com.sillykid.app.R;
 
 
@@ -9,9 +14,73 @@ import com.sillykid.app.R;
  */
 public class LineDetailsPayOrderActivity extends BaseActivity {
 
+    @BindView(id = R.id.img_lineDetails)
+    private ImageView img_lineDetails;
+
+    @BindView(id = R.id.tv_adult)
+    private TextView tv_adult;
+
+    @BindView(id = R.id.tv_children)
+    private TextView tv_children;
+
+    @BindView(id = R.id.tv_luggage)
+    private TextView tv_luggage;
+
+    @BindView(id = R.id.tv_travelTime)
+    private TextView tv_travelTime;
+
+    @BindView(id = R.id.tv_contact)
+    private TextView tv_contact;
+
+    @BindView(id = R.id.tv_contactWay)
+    private TextView tv_contactWay;
+
+    @BindView(id = R.id.tv_priceDescription)
+    private TextView tv_priceDescription;
+
+    @BindView(id = R.id.tv_dueThat)
+    private TextView tv_dueThat;
+
+    @BindView(id = R.id.tv_remark)
+    private TextView tv_remark;
+
+    @BindView(id = R.id.tv_orderPriceNoSign)
+    private TextView tv_orderPriceNoSign;
+
+    @BindView(id = R.id.tv_vouchers)
+    private TextView tv_vouchers;
+
+
+    @BindView(id = R.id.tv_confirmPayment, click = true)
+    private TextView tv_confirmPayment;
+
     @Override
     public void setRootView() {
         setContentView(R.layout.activity_linedetailspayorder);
     }
+
+    @Override
+    public void initData() {
+        super.initData();
+    }
+
+    @Override
+    public void initWidget() {
+        super.initWidget();
+    }
+
+    @Override
+    public void widgetClick(View v) {
+        super.widgetClick(v);
+        switch (v.getId()) {
+            case R.id.tv_vouchers:
+
+                break;
+            case R.id.tv_confirmPayment:
+
+                break;
+        }
+    }
+
 
 }

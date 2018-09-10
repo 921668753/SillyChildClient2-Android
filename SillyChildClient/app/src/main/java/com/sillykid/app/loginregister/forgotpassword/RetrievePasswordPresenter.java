bf4e3f4ed5508c536022c9556dd105106778e355
921668753 <921668753@qq.com>
2018-09-10 15:04:39 +0800

    fix BUG

--- SillyChildClient/app/src/main/java/com/sillykid/app/loginregister/forgotpassword/RetrievePasswordPresenter.java
+++ SillyChildClient/app/src/main/java/com/sillykid/app/loginregister/forgotpassword/RetrievePasswordPresenter.java
@@ -29,10 +29,10 @@ public class RetrievePasswordPresenter implements RetrievePasswordContract.Prese
             mView.errorMsg(KJActivityStack.create().topActivity().getString(R.string.hintPhoneText), 0);
             return;
         }
-//        if (phone.length() != 11) {
-//            mView.errorMsg(KJActivityStack.create().topActivity().getString(R.string.hintPhoneText1), 0);
-//            return;
-//        }
+        if (!countryCode.equals("86") && phone.startsWith("0")) {
+            mView.errorMsg(KJActivityStack.create().topActivity().getString(R.string.hintPhoneText2), 0);
+            return;
+        }
         HttpParams httpParams = HttpUtilParams.getInstance().getHttpParams();
         httpParams.put("mobile", phone);
         httpParams.put("country_code", countryCode);
--- SillyChildClient/app/src/main/java/com/sillykid/app/loginregister/register/RegisterPresenter.java
+++ SillyChildClient/app/src/main/java/com/sillykid/app/loginregister/register/RegisterPresenter.java
@@ -46,10 +46,10 @@ public class RegisterPresenter implements RegisterContract.Presenter {
             mView.errorMsg(KJActivityStack.create().topActivity().getString(R.string.hintPhoneText), 0);
             return;
         }
-//        if (phone.length() != 11) {
-//            mView.errorMsg(KJActivityStack.create().topActivity().getString(R.string.hintPhoneText1), 0);
-//            return;
-//        }
+        if (!countryCode.equals("86") && phone.startsWith("0")) {
+            mView.errorMsg(KJActivityStack.create().topActivity().getString(R.string.hintPhoneText2), 0);
+            return;
+        }
         HttpParams httpParams = HttpUtilParams.getInstance().getHttpParams();
         httpParams.put("mobile", phone);
         httpParams.put("country_code", countryCode);

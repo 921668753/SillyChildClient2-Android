1f15505b03d8b5f9c6ef9f0354f067e787ee0d05
921668753 <921668753@qq.com>
2018-09-10 17:30:34 +0800

    fix BUG

--- SillyChildClient/app/src/main/java/com/sillykid/app/constant/URLConstants.java
+++ SillyChildClient/app/src/main/java/com/sillykid/app/constant/URLConstants.java
@@ -20,7 +20,7 @@ public class URLConstants {
     // public static String SERVERURL = "http://xiaowei.local.keiousoft.com/";
     //  public static String SERVERURL = "http://192.168.1.247:8080/";
     public static String SERVERURL = "http://user.api.shahaizhi.com/";
-    public static String SERVERURL1 = "http://www.shahaizhi.com/";
+    public static String SERVERURL1 = "http://www.shahaizhi.tech/";
 
     /**
      * 请求地址URL

777fcc1d99e9a302fcaadf0700de1a36bcca98f3
921668753 <921668753@qq.com>
2018-09-10 17:28:48 +0800

    fix BUG

--- SillyChildClient/app/src/main/java/com/sillykid/app/constant/URLConstants.java
+++ SillyChildClient/app/src/main/java/com/sillykid/app/constant/URLConstants.java
@@ -18,9 +18,9 @@ public class URLConstants {
      * 测试服务器地址URL
      */
     // public static String SERVERURL = "http://xiaowei.local.keiousoft.com/";
-  //  public static String SERVERURL = "http://192.168.1.247:8080/";
-    public static String SERVERURL = "http://user.api.shahaizi.tech/";
-    public static String SERVERURL1 = "http://www.shahaizhi.tech/";
+    //  public static String SERVERURL = "http://192.168.1.247:8080/";
+    public static String SERVERURL = "http://user.api.shahaizhi.com/";
+    public static String SERVERURL1 = "http://www.shahaizhi.com/";
 
     /**
      * 请求地址URL
